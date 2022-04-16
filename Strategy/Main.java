3e7fc708e55af9255c2da4c13e7fbda7dcc1bade
Claudio Munoz <claudio.munoz.m@usach.cl>
2022-04-16 18:55:18 -0400

    Se implementa la funcionalidad para sumar dos numeros

--- Strategy/Main.java
+++ Strategy/Main.java
@@ -7,5 +7,17 @@ public class Main {
 		System.out.println("----------------------------------------");
 		System.out.println("Patrón Strategy");
 		System.out.println("----------------------------------------");
+		System.out.println ("Por favor introduzca el primer numero a operar:");
+		int number1, number2;
+        Scanner entrada = new Scanner(System.in);
+        number1 = Integer.parseInt(entrada.nextLine());
+		System.out.println ("Por favor introduzca el segundo numero a operar:");
+		number2 = Integer.parseInt(entrada.nextLine());
+		System.out.println("----------------------------------------");
+		entrada.close();
+		int numbers[] = {number1, number2};
+		CalculatorContext context = new CalculatorContext();
+		context.setCalculatorMethod(new Addition());
+		context.calculateResult(numbers);
 	}
 }
